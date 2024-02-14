568fce101559f9f04f2dafbc5aaedad2d146a3f0
Savo Bajic <bajic.savo@gmail.com>
2024-02-13 20:57:27 -0500

    Updated configuration to properly upload code

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 
-const int pin = 1;
+const int pin = PIN_PD1;
 
 void setup() {
   pinMode(pin, OUTPUT);
@@ -8,7 +8,7 @@ void setup() {
 
 void loop() {
   digitalWrite(pin, HIGH);
-  delay(500);
+  delay(2000);
   digitalWrite(pin, LOW);
-  delay(1000);
+  delay(2000);
 }
